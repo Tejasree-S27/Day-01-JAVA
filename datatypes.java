aa5145030b7febfdb89a09f3bb1148af262119ce
Tejasree-S27 <stejasree348@gmail.com>
2024-12-02 14:45:03 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class datatypes {
+    public static void main(String args[])
+    {
+        int a=23;
+        char b='S';
+        float c=43.45f;
+        double d=36.52000;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
