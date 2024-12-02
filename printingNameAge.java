dfbf4a2b128f665528c431124d2a5e21d387d78f
Tejasree-S27 <stejasree348@gmail.com>
2024-12-02 16:23:07 +0530

    Add files via upload

---
+++ printingNameAge.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class printingNameAge
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int b=sc.nextInt();
+        System.out.println("The name of the person is "+a+" and age is "+b);
+    }
+}
